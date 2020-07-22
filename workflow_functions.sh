ade4b1634a9b12501d3977f90afc00415fd930bf
Okadarien Saru <okadas@dev.sysnk.net>
2020-07-23 07:10:04 +0900

    Archlinux workflow added

--- workflow_functions.sh
+++ workflow_functions.sh
@@ -10,7 +10,7 @@ set_image_name(){
 }
           
 build_image(){ 
-  for coshapp_ver in 10.4-slim; do 
+  for coshapp_ver in ${coshapp_ver:-10.4-slim}; do 
     for j in $@; do
       export coshapp_ver;
       set_image_name $j ;
